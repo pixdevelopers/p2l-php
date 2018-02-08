9fc4cd1ddf662e0d9e29c81364f385a495ed6396
Mamo YZ <yzmamo@gmail.com>
2018-02-08 18:33:10 +0330

    updates

--- config/common-config.js
+++ config/common-config.js
@@ -7,7 +7,7 @@
             delete $httpProvider.defaults.headers.common["X-Requested-With"];
             $httpProvider.defaults.headers.common["Accept"] = "application/json";
             $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
-            //$locationProvider.html5Mode(true);
+            locationProvider.html5Mode(true);
             $urlRouterProvider.otherwise('/');
            
         }]);
